5c2abfdbb4755dbb3e3ca18ed3783d9d2d8ed4f0
Jonas Achouri Sihlen <jonasasihlen@gmail.com>
2022-07-13 10:17:57 +0200

    Adding a constructor to the language class.

--- src/Language.java
+++ src/Language.java
@@ -5,6 +5,13 @@ public Language {
     protected String regionsSpoken;
     protected String wordOrder;
 
+    Language (String language, int speakers, String regions, String order) {
+        this.name = language;
+        this.numSpeakers = speakers;
+        this.regionsSpoken = regions;
+        this.wordOrder = order;
+        }
+
     public static void main(String[]args){
 
         }
