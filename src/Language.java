fb483e4f5d9ac70accaacd387918ce70e48b7ec6
Jonas Achouri Sihlen <jonasasihlen@gmail.com>
2022-07-14 07:45:06 +0200

    Fixing errors.

--- src/Language.java
+++ src/Language.java
@@ -1,4 +1,4 @@
-public class Language {
+class Language {
 
     protected String name;
     protected int numSpeakers;
@@ -10,17 +10,18 @@ public class Language {
         this.numSpeakers = speakers;
         this.regionsSpoken = regions;
         this.wordOrder = order;
-        }
+    }
+    public void getInfo() {
 
-        public void getInfo() {
-
-        System.out.println(this.name + "is spoken by " +  this.numSpeakers + "people mainly in " + this.regionsSpoken"." + "The language follows the word order: " + this.wordOrder);
-
-        }
-
+        System.out.println(this.name + " is spoken by " +  this.numSpeakers + " people, mainly in " + this.regionsSpoken + ". The language follows the word order: " + this.wordOrder);
+    }
     public static void main(String[]args){
 
-        Language French = new Language("French", 321000000, "Europe, Africa, Canada, Asia, Middle East");
-        French.getInfo();
-        }
+        Language french = new Language("French", 321000000, "Europe, Africa, Canada, Asia, Middle East", "subject-verb-object");
+        french.getInfo();
+    }
+
+
+
 }
+
